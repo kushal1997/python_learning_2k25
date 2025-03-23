4d40a63296a389163bb4504c8dfe5633ca5e9b1c
kushal1997 <kushalrao103@gmail.com>
2025-03-23 12:08:04 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,18 @@
+a=2
+b=5
+c=1
+
+if a>b and a>c:
+    print(" a as",a," is the largest number")
+else:
+    if b > c:
+        print("b as",b," is the largest number")
+    else:
+        print("c as",c," is the largest number")
+
+if a<b and a<c:
+        print("a as",a," is the smallest number")
+elif b<a and b<c:
+         print("b as",b," is the smallest number")
+else:
+      print("c as",c," is the smallest number")
