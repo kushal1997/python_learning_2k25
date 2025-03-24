f100ed9dc1b3ee03dcf672a868ae3ce8b3ca19ab
kushal1997 <kushalrao103@gmail.com>
2025-03-24 11:13:12 +0530

    tuple done

---
+++ 2nd calss_ array_sets_OOPs/4tuple.py
@@ -0,0 +1,9 @@
+# Tuple is an ordered collection of elements in programming. It is immutable, meaning its elements cannot be changed once created.
+# Tuples can store different data types, and their elements can be accessed through indexing.
+# They are useful for representing fixed sets of values or returning multiple values from a function.
+
+p_data = ("Kushal", 27, "INDIA")
+
+print(p_data)
+
+print(p_data[0])
