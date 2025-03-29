01ab10565d05bff88ca2933362332dd8497ffaf9
kushal1997 <kushalrao103@gmail.com>
2025-03-29 15:22:37 +0530

    how to use params

--- 3rd_class_flask/app.py
+++ 3rd_class_flask/app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+arr =[]
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+#using params
+@app.route('/name/<username>')
+def name(username):
+    arr.append(username)
+    return username + " added successfully"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
