0a62b017782ce96020b2072a2e327fef4a62d087
kushal1997 <kushalrao103@gmail.com>
2025-04-01 00:03:17 +0530

    sign up form linked to mongo bd

--- 3rd_class_flask/login_signup_mongo_app.py
+++ 3rd_class_flask/login_signup_mongo_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -12,11 +12,35 @@ except Exception as e:
 
 @app.route("/")
 def login():
-    data = {
-        "name" : "kushal"
-    }
-    collection.insert_one(data)
     return render_template('login.html')
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template('signup.html')
+
+@app.route("/home")
+def home():
+    return "Login successfully"
+
+@app.route("/submit-login", methods=["POST"])
+def submit_login():
+    return "agsa"
+
+@app.route("/submit-signup", methods=["POST"])
+def submit_sign_up():
+    data = request.form
+    print("data --------------->",data)
+    password = data.get("password","")
+    username = data.get("username","")
+
+    if not password or not username:
+        return jsonify({
+            "message" : "password & username is mandatory"
+        }),401
+    user_data = {key: value for key, value in data.items()}
+    collection.insert_one(user_data)
+    
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(debug= True)
