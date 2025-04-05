0d646fca73968b8e4350be31c92da146d3a5e448
kushal1997 <kushalrao103@gmail.com>
2025-04-05 14:05:40 +0530

    updated add task with some features

--- 3rd_class_flask/6_todo_list.py
+++ 3rd_class_flask/6_todo_list.py
@@ -4,6 +4,10 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from datetime import datetime
 
+import time
+import random
+import string
+
 app = Flask(__name__)
 
 try:
@@ -13,19 +17,12 @@ try:
 except Exception as e:
     print(f"erros is -==========================> {e}")
 
-@app.route("/")
-def home():
-    return "Home Page"
+def generate_unique_id():
+    timestamp = str(int(time.time()))  # current time in seconds
+    random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=4))  # 4-char random string
+    return timestamp + random_str
 
-@app.route("/task", methods = ["POST"])
-def add_task():
-    data = request.json
-    required_fields = ['task', 'date', 'time']
-
-    if not data or not all(field in data and data[field] for field in required_fields):
-        return jsonify({
-            "message": "All fields (task, date, time, status) are required"
-        }), 400
+def check_date_time(data):
     
     task_date = datetime.strptime(data['date'], '%d/%m/%Y').date()
     today = datetime.today().date()
@@ -44,17 +41,36 @@ def add_task():
             data['status'] = 'due'
         else:
             data["status"] = 'pending'
+    
+    return data
 
+@app.route("/")
+def home():
+    return "Home Page"
+
+@app.route("/task", methods = ["POST"])
+def add_task():
+    data = request.json
+    required_fields = ['task', 'date', 'time']
+
+    if not data or not all(field in data and data[field] for field in required_fields):
+        return jsonify({
+            "message": "All fields (task, date, time, status) are required"
+        }), 400
+    
+    updated_data = check_date_time(data)
+    updated_data['id'] = generate_unique_id()
     # all_tasks = list(collection.find({},{'_id':0}))
     d = collection.find_one({'task': data['task']})
     print(d)
 
     if d :
-        return jsonify({
-            'message' : "task already in database"
-        }),409
+        if data['time'] == d["time"] and data["date"] == d["date"]:
+            return jsonify({
+                'message' : "task already in database"
+            }),409
     
-    collection.insert_one(data)
+    collection.insert_one(updated_data)
     
     return jsonify({
         "message" : "task added succesfully",
